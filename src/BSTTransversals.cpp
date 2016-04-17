f31d9ca917f9d73d4cbd7f574042daced108b0a9
chandu54 <chandu54@missionrnd.com>
2016-04-17 11:57:10 +0530

    Submit Commit

--- src/BSTTransversals.cpp
+++ src/BSTTransversals.cpp
@@ -15,21 +15,91 @@ it and understand how testing works .
 */
 
 #include <stdio.h>
-
+#include<malloc.h>
 struct node{
 	struct node * left;
 	int data;
 	struct node *right;
 };
+struct node * new_node(int data){
+	struct node *temp = (struct node *)malloc(sizeof(struct node));
+	temp->data = data;
+	temp->left = NULL;
+	temp->right = NULL;
+	return temp;
+}
+struct node * add_node(struct node *root, int data){
+	if (root == NULL) return new_node(data);
 
+	if (data < root->data)
+		root->left = add_node(root->left, data);
+	else if (data > root->data)
+		root->right = add_node(root->right, data);
+
+	return root;
+}
+
+void inorderFunction(struct node *root, int *arr, int *index)
+{
+
+	if (!root)
+	{
+		return;
+	}
+	else
+	{
+		inorderFunction(root->left, arr, index);
+		arr[*index] = root->data;
+		(*index)++;
+		inorderFunction(root->right, arr, index);
+	}
+}
 
 void inorder(struct node *root, int *arr){
-	
+	int index = 0;
+	if (root != NULL && arr != NULL)
+		inorderFunction(root, arr, &index);
+	else
+		return;
+}
+void preorderFunction(struct node *root, int *arr, int *index)
+{
+	if (!root)
+	{
+		return;
+	}
+	else
+	{
+		arr[*index] = root->data;
+		(*index)++;
+		preorderFunction(root->left, arr, index);
+		preorderFunction(root->right, arr, index);
+	}
 }
 void preorder(struct node *root, int *arr){
-	
+	int index = 0;
+	if (root != NULL && arr != NULL)
+		preorderFunction(root, arr, &index);
+	else
+		return;
+}
+void postorderFunction(struct node *root, int *arr, int *index)
+{
+
+	if (!root)
+		return;
+	else
+	{
+		postorderFunction(root->left, arr, index);
+		postorderFunction(root->right, arr, index);
+		arr[*index] = root->data;
+		(*index)++;
+	}
 }
 void postorder(struct node *root, int *arr){
-	
+	int index = 0;
+	if (root != NULL && arr != NULL)
+		postorderFunction(root, arr, &index);
+	else
+		return;
 }
-
